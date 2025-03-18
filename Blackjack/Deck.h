7f0904b608f65ed9fddba64907d0b0cf02a246a0
Elektromannen <Ottar.rolfsrud@hotmail.com>
2025-03-18 15:38:13 +0100

    Added deck building and shuffeling

--- Blackjack/Deck.h
+++ Blackjack/Deck.h
@@ -1,41 +1,72 @@
 /*This is where we define the playing card deck*/
 #include <stdint.h>
 #include <string.h>
+#include <time.h>
 
+char* card[13] = {
+    "Ace ",
+    "Two ",
+    "Three ",
+    "Four ",
+    "Five ",
+    "Six ",
+    "Seven ",
+    "Eight ",
+    "Nine ",
+    "Ten ",
+    "Jack ",
+    "Queen ",
+    "King "};
 
-const char* card[13] = {
-    "Ace",
-    "Two",
-    "Three",
-    "Four",
-    "Five",
-    "Six",
-    "Seven",
-    "Eight",
-    "Nine",
-    "Ten",
-    "Jack",
-    "Queen",
-    "King"};
-
-const char* suit[4] = {
+char* suit[4] = {
     "Hearts",
-    "Diamons",
+    "Diamonds",
     "Clubs",
     "Spades"};
 
 char* deck[52];
 
+// runs card 13 times for each suit using for i suit < 4 with for card < 13
 void init_deck(){
-    for(int i = 0; i < sizeof(deck); i++){
-        deck[i] = "s";
+    int i = 0;
+    for(int s = 0; s < sizeof(suit)/sizeof(suit[0]); s++){
+        for(int c = 0; c < sizeof(card)/sizeof(card[0]); c++){
+            deck[i] = malloc(20);
+            strcat(deck[i],card[c]);
+            strcat(deck[i],suit[s]);
+            i++;
+            }
     }
+    printf("Deck is made\n");
 }
 
 void deck_print(){
     printf("======== Deck ========\n");
     for(int8_t i = 0; i < sizeof(deck)/sizeof(deck[0]); i++){
-        printf("%s \n", suit[i]);
+        printf("%s \n", deck[i]);
+    }
+}
+
+void deck_pull_card(){
+    int choice = 0;
+    printf("Which of the 52 cards do you want to pull?\n");
+    printf("Your input: ");
+    scanf("%d",&choice);
+    if ((choice > 0)&(choice < 53)){
+        printf("Your card is %s\n",deck[choice-1]);
+    }
+    else{
+        printf("There are not %s many cards in the deck\n",deck[choice-1]);
+    }
+}
+
+void deck_shuffle(){
+    srand(time(NULL));
+    for (int i = 0; i < sizeof(deck)/sizeof(deck[0]); ++i){
+    int j = rand() % (sizeof(deck)/sizeof(deck[0])-i) + i;
+    char* temp = deck[i];
+    deck[i] = deck[j];
+    deck[j] = temp;
     }
 }
 
--- Blackjack/Game.c
+++ Blackjack/Game.c
@@ -9,11 +9,29 @@ Blackjack game made by me for to help me learn and get used to programming in C
 #include <stdlib.h>
 #include "Deck.h" //Handles the Deck building
 
+void logo(){
+    printf(" ______   _        _______  _______  _       _________ _______  _______  _       \n");
+    printf("(  ___ \\ ( \\      (  ___  )(  ____ \\| \\    /\\\\__    _/(  ___  )(  ____ \\| \\    /\\\n");
+    printf("| (   ) )| (      | (   ) || (    \\/|  \\  / /   )  (  | (   ) || (    \\/|  \\  / /\n");
+    printf("| (__/ / | |      | (___) || |      |  (_/ /    |  |  | (___) || |      |  (_/ /\n");
+    printf("|  __ (  | |      |  ___  || |      |   _ (     |  |  |  ___  || |      |   _ ( \n");
+    printf("| (  \\ \\ | |      | (   ) || |      |  ( \\ \\    |  |  | (   ) || |      |  ( \\ \\ \n");
+    printf("| )___) )| (____/\\| )   ( || (____/\\|  /  \\ \\|\\_)  )  | )   ( || (____/\\|  /  \\ \\\n");
+    printf("|/ \\___/ (_______/|/     \\|(_______/|_/    \\/(____/   |/     \\|(_______/|_/    \\/\n");
+}
 
 void menu(){
     int8_t loop = 1;
     while (loop == 1){
         printf("====== Menu ====== \nPlease choose an option:\n");
+        printf("1. Play\n");
+        printf("2. Balance\n");
+        printf("3. \n");
+        printf("4. \n");
+        printf("5. \n");
+        printf("6. Shuffle deck\n");
+        printf("7. Pull a card from the deck\n");
+        printf("8. View deck\n");
         printf("9. see all cards\n");
         printf("0. Quit\n");
         
@@ -22,23 +40,37 @@ void menu(){
         scanf("%d",&choice);
 
         switch (choice){
-        case 9:
-            print_cards();
-            break;
-        
-        case 0:
-            loop = 0;
-            printf("Quitting\n");
-            break;
-        
-        default:
-            printf("Not a choice\n\n");
-            break;
+            case 6:
+                deck_shuffle();
+                break;
+
+            case 7:
+                deck_pull_card();
+                break;
+
+            case 8:
+                deck_print();
+                break;
+            
+            case 9:
+                print_cards();
+                break;
+            
+            case 0:
+                loop = 0;
+                printf("Quitting\n");
+                break;
+            
+            default:
+                printf("Not a choice\n\n");
+                break;
         }
     }
 }
 
 int main(){
+    logo();
+    init_deck();
     menu();
     return 0;
 }

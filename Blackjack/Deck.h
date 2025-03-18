39d6d72d3e8b8e2b9a79a607fe1495666e5955f1
Elektromannen <Ottar.rolfsrud@hotmail.com>
2025-03-18 05:47:10 +0100

    Added menu and started work on deck building

--- Blackjack/Deck.h
+++ Blackjack/Deck.h
@@ -1,1 +1,52 @@
 /*This is where we define the playing card deck*/
+#include <stdint.h>
+#include <string.h>
+
+
+const char* card[13] = {
+    "Ace",
+    "Two",
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight",
+    "Nine",
+    "Ten",
+    "Jack",
+    "Queen",
+    "King"};
+
+const char* suit[4] = {
+    "Hearts",
+    "Diamons",
+    "Clubs",
+    "Spades"};
+
+char* deck[52];
+
+void init_deck(){
+    for(int i = 0; i < sizeof(deck); i++){
+        deck[i] = "s";
+    }
+}
+
+void deck_print(){
+    printf("======== Deck ========\n");
+    for(int8_t i = 0; i < sizeof(deck)/sizeof(deck[0]); i++){
+        printf("%s \n", suit[i]);
+    }
+}
+
+void print_cards(){
+    printf("======== The suits are: ========\n");
+    for(int8_t i = 0; i < sizeof(suit)/sizeof(suit[0]); i++){
+        printf("%s \n", suit[i]);
+    }
+    printf("======== The cards are: ========\n");
+    for(int8_t i = 0; i < sizeof(card)/sizeof(card[0]); i++){
+        printf("%s \n", card[i]);
+    }
+}
+
--- Blackjack/Game.c
+++ Blackjack/Game.c
@@ -1,10 +1,44 @@
+/*
 
+Author: Borgas
+Purpose:
+Blackjack game made by me for to help me learn and get used to programming in C
+*/
 
 #include <stdio.h> //Library for Input Output handling
+#include <stdlib.h>
 #include "Deck.h" //Handles the Deck building
 
 
-int main(){
+void menu(){
+    int8_t loop = 1;
+    while (loop == 1){
+        printf("====== Menu ====== \nPlease choose an option:\n");
+        printf("9. see all cards\n");
+        printf("0. Quit\n");
+        
+        int choice = 0;
+        printf("Your input: ");
+        scanf("%d",&choice);
 
+        switch (choice){
+        case 9:
+            print_cards();
+            break;
+        
+        case 0:
+            loop = 0;
+            printf("Quitting\n");
+            break;
+        
+        default:
+            printf("Not a choice\n\n");
+            break;
+        }
+    }
+}
+
+int main(){
+    menu();
     return 0;
 }
